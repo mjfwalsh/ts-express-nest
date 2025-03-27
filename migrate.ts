cd066b2f173f66a73ec256d13bc155cedb38cd60
Michael J. Walsh <mjfwalsh@gmail.com>
2025-03-27 17:59:38 +0000

    Put some typescript code in migrate.ts

--- migrate.ts
+++ migrate.ts
@@ -1,6 +1,10 @@
 // Configuration file example for ts-migrate-mongoose:
 // You can also use .env instead of a config file
 
+const msg: string = "Hello World";
+
+console.log(msg);
+
 export default {
   uri: process.env.MIGRATE_MONGO_URI ?? 'mongodb://localhost:27017/nest',
   // "collection": "migrations",
